3cf360dff842feb81a511f2ba752f8d0fa75d6a7
Yonpark <yw9142@gmail.com>
2020-09-27 02:17:32 +0900

    3-14 마크다운

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,8 +1,16 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.filter
-def sub(value, arg): # 기존 값(value)에서 입력으로 받은 값(arg)을 빼서 리턴하는 필터
+def sub(value, arg):  # 기존 값(value)에서 입력으로 받은 값(arg)을 빼서 리턴하는 필터
     return value - arg
+
+
+@register.filter()
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
